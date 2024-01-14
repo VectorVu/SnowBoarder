91fdbf50b045c996e50e3634d3fb59482944d270
VictorVu <vuhoangtruong1998@gmail.com>
2024-01-14 19:40:48 +0700

    Added game functions

--- Script/PlayerController.cs
+++ Script/PlayerController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 3f;
+    int EffectTime = 2000;
+    bool OnEffectTime = false;
+    [SerializeField] ParticleSystem HighEffect;
+    [SerializeField] ParticleSystem LowEffect;
+
     Rigidbody2D rd2d;
 
     // Start is called before the first frame update
@@ -23,6 +28,36 @@ public class PlayerController : MonoBehaviour
         else if(Input.GetKey(KeyCode.RightArrow)) 
         {
             rd2d.AddTorque(-torqueAmount);
+        }
+        if (OnEffectTime) 
+        {
+            EffectTime--;
+        }
+        if (EffectTime < 0)
+        {
+            OnEffectTime = false;
+            torqueAmount = 3f;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "HighCloud") 
+        {
+            EffectTime = 2000;
+            OnEffectTime = true;
+            HighEffect.Play();
+            LowEffect.Stop();
+            torqueAmount = 10f;
+        }
+
+        if (other.tag == "PoisonCloud") 
+        {
+            EffectTime = 2000;
+            OnEffectTime = true;
+            LowEffect.Play();
+            HighEffect.Stop();
+            torqueAmount = 0.5f;
         }
     }
 }
